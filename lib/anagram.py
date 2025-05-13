609d4ff668e2345cfc1d2f0ea9ea292e189c8452
CliffLinusthomi <linusthomi2@gmail.com>
2025-05-13 11:41:38 +0300

    first commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,26 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    @property
+    def word(self):
+        return self._word
+    
+    @word.setter  
+    def word(self, value):
+        if isinstance(value, str):
+            self._word = value.lower()
+        else:
+            raise ValueError("Word must be a string")
+
+    def match(self, word_list):
+            matches = []
+            for canditate in word_list:
+                if sorted(canditate.lower()) == sorted(self.word.lower()) and canditate.lower() != self.word.lower():
+                    matches.append(canditate)
+            return matches
+            
+                
+        
+        
+
